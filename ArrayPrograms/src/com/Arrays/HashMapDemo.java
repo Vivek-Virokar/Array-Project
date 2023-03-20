d3495eac20f4b1d6e76cb9accb8bc9971d00cd35
Vivek <viveksvirokar1106@gmail.com>
2023-03-20 11:22:57 +0530

    adding the HashMap file

---
+++ ArrayPrograms/src/com/Arrays/HashMapDemo.java
@@ -0,0 +1,19 @@
+package com.Arrays;
+
+import java.util.HashMap;
+
+public class HashMapDemo {
+	public static void main(String args[])
+	{
+		HashMap<String,Integer> map=new HashMap<>();
+		map.put("Vivek", 1);
+		map.put("Vaishnavi", 2);
+		map.put("Nitesh", 3);
+		System.out.println(map);
+		
+		
+		
+		
+	}
+
+}
